57f05e1f48f5c52415b5779295ccbc2900764a2d
Lisa Saalfrank <lsaalfrank@hof-university.de>
2018-06-01 18:34:46 +0200

    Add HTTP abstraction base

---
+++ src/Http/Request.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wildgame\Http;
+
+/**
+ * Abstraction layer for HTTP Requests. Stores the relevant data in an easily
+ * manipulatable or mockable way without affecting the original Request.
+ *
+ * @author      Lisa Saalfrank <lisa.saalfrank@web.de>
+ * @copyright   2018 Lisa Saalfrank
+ */
+class Request {
+
+}
---
+++ src/Http/Response.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wildgame\Http;
+
+/**
+ * Abstraction layer for a HTTP Response. Stores the relevant data so that it is
+ * both manipulatable and flexible.
+ *
+ * @author      Lisa Saalfrank <lisa.saalfrank@web.de>
+ * @copyright   2018 Lisa Saalfrank
+ */
+class Response {
+
+}
