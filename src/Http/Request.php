ab4d64b47a0a0e1813791496e6f4d18bc6eec604
Lisa Saalfrank <lsaalfrank@hof-university.de>
2018-06-02 15:53:07 +0200

    Define outline

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -62,6 +62,61 @@ class Request {
     }
 
     /**
+     * Creates a new instance of Request based on the information extracted from
+     * the global variables $_SERVER, $_GET, $_POST and $_SESSION. (Factory)
+     *
+     * @return  \Wildgame\Http\Request
+     */
+    public static function createFromGlobals() : Request {
+        // Add code here
+    }
+
+    /**
+     * @param   string  $method
+     *
+     * @return  \Wildgame\Http\Request
+     */
+    public function withMethod(string $method) : Request {
+        // Add code here
+    }
+
+    /**
+     * @param   bool    $ajax
+     *
+     * @return  \Wildgame\Http\Request
+     */
+    public function withAjax(bool $ajax) : Request {
+        // Add code here
+    }
+
+    /**
+     * @param   \Wildgame\Http\Uri  $uri
+     *
+     * @return  \Wildgame\Http\Request
+     */
+    public function withUri(Uri $uri) : Request {
+        // Add code here
+    }
+
+    /**
+     * @param   \Wildgame\Http\Input    $input
+     *
+     * @return  \Wildgame\Http\Request
+     */
+    public function withInput(Input $input) : Request {
+        // Add code here
+    }
+
+    /**
+     * @param   \Wildgame\Http\Session  $session
+     *
+     * @return  \Wildgame\Http\Request
+     */
+    public function withSession(Session $session) : Request {
+        // Add code here
+    }
+
+    /**
      * Returns the original URI object instead of the URI string.
      * For retrieving just the string use Request::uri().
      *
@@ -69,6 +124,27 @@ class Request {
      */
     public function getUri() : Uri {
         return $this->uri;
+    }
+
+    /**
+     * @return  \Wildgame\Http\Input
+     */
+    public function getInput() : Input {
+        return $this->input;
+    }
+
+    /**
+     * @return  \Wildgame\Http\Session
+     */
+    public function getSession() : Session {
+        return $this->session;
+    }
+
+    /**
+     * @return  \Wildgame\Http\Input
+     */
+    public function getInput() : Input {
+        return $this->input;
     }
 
     /**
---
+++ web/index.php
@@ -0,0 +1,88 @@
+<?php
+
+use Wildgame\Wildgame;
+
+use Wildgame\Http\Request;
+use Wildgame\Http\Response;
+
+use Wildgame\Utility\Container;
+
+/**
+ * Entry point for every HTTP Request sent to the server.
+ *
+ * @author      Lisa Saalfrank <lisa.saalfrank@web.de>
+ * @copyright   2018 Lisa Saalfrank
+ */
+
+/* -----------------------------------------------------------------------------
+ * Setup of autoloading
+ * -------------------------------------------------------------------------- */
+
+spl_autoload_register(function ($class) {
+    $class = substr($class, strlen('Wildgame\\'));
+    include __DIR__ .'/../src/' . str_replace('\\', '/', $class) . '.php';
+});
+
+/* -----------------------------------------------------------------------------
+ * Basic HTTP Setup
+ * -------------------------------------------------------------------------- */
+
+// Create the HTTP Request from the global variables
+$request = Request::createFromGlobals();
+// Create a default HTTP Response with text/html and the 200 OK status
+$response = Response::createDefaultHtml();
+
+/* -----------------------------------------------------------------------------
+ * Registering of dependencies
+ * -------------------------------------------------------------------------- */
+
+$container = new Container();
+
+$container->register('PagesController',
+function(Request $req, Response $res, Container $c) {
+    return new \Wildgame\Controller\PagesController($req, $res, $c);
+});
+
+$container->register('ErrorController',
+function(Request $req, Response $res, Container $c) {
+    return new \Wildgame\Controller\ErrorController($req, $res, $c);
+});
+
+/* -----------------------------------------------------------------------------
+ * Bootstraping
+ * -------------------------------------------------------------------------- */
+
+// New instance of the Wilder Reiter game master class
+$app = new Wildgame($request, $response, $container);
+
+/* -----------------------------------------------------------------------------
+ * Route configuration
+ * -------------------------------------------------------------------------- */
+
+$app->serverError('ErrorController@serverError');
+$app->notFoundError('ErrorController@notFoundError');
+
+/* -----------------------------------------------------------------------------
+ * Registering of routes
+ * -------------------------------------------------------------------------- */
+
+// Home page
+$app->get('/', 'PagesController@showHome');
+
+// Guest pages
+$app->get('/preview', 'PagesController@showPreview');
+$app->get('/support', 'PagesController@showSupport');
+$app->get('/news/?{page:num}', 'PagesController@showNews');
+
+// Footer pages
+$app->get('/privacy', 'PagesController@showPrivacy');
+$app->get('/rules', 'PagesController@showRules');
+$app->get('/about', 'PagesController@showAbout');
+$app->get('/contact', 'PagesController@showContact');
+$app->get('/legal', 'PagesController@showLegal');
+
+/* -----------------------------------------------------------------------------
+ * Dispatching
+ * -------------------------------------------------------------------------- */
+
+$app->respond();
