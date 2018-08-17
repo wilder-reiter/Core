f6cdcaa5d9410ab1838d2c24a70d0bc218aaba49
Lisa Saalfrank <lsaalfrank@hof-university.de>
2018-08-17 04:41:03 +0200

    Add creation of Definitions

--- src/Container/Container.php
+++ src/Container/Container.php
@@ -13,31 +13,45 @@ class Container {
     /**
      * @var array
      */
-    private $collection = [];
+    protected $definitions = [];
 
     /**
+     * Create and add a definition to the container. The defenition will be
+     * returned after its creation for the purpose of modifying it.
+     *
      * @param   string  $alias
      * @param   string  $class
      *
      * @return  \Wildgame\Container\Definition
      */
-    public function add(string $alias, string $class = null) : Definition {
-        // Add code here
+    public function add(string $alias, string $class = null) : Definition
+    {
+        // If no classname was provided, it is identical to the alias
+        $class = $class ?? $alias;
+
+        // Create a new Definition instance and add it to the collection
+        $definition = new Definition($class);
+        $this->definitions[$alias] = $definition;
+
+        // Return the definition, so that it can be modified afterwards
+        return $this->definition[$alias];
     }
 
     /**
-     * @param   string  $id
+     * @param   string  $alias
+     *
+     * @return  mixed
      */
-    public function get(string $id) {
+    public function get(string $alias) {
         // Add code here
     }
 
     /**
-     * @param   string  $id
+     * @param   string  $alias
      *
      * @return  bool
      */
-    public function has(string $id) : bool {
+    public function has(string $alias) : bool {
         // Add code here
     }
 }
