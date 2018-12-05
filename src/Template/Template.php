0c82c01f7642a83dd59ba23a1b6025d42c96f453
Lisa Saalfrank <lisa.saalfrank@hof-university.de>
2018-12-05 16:33:38 +0100

    Added variables to engine

--- src/Template/Template.php
+++ src/Template/Template.php
@@ -5,16 +5,16 @@ namespace Wildgame\Template;
 /**
  * Replaces placeholders in a given string with the concrete values.
  * Allows comments that will be deleted from the string after rendering.
- * Also implements conditional printing of values.
  *
  * @author      Lisa Saalfrank <lisa.saalfrank@web.de>
  * @copyright   2018 Lisa Saalfrank
  */
 class Template {
 
-    private $variable = "\{\{[a-z]+\}\}";
-    private $comment = "\{\{\*\}\}.+\{\{\/\*\}\}";
-    private $conditional = "\{\{[a-z]+\?[a-z]+\}\}";
+    /**
+     * @var string
+     */
+    private $comment = "#\{\{\*\}\}.+\{\{\/\*\}\}#";
 
     /**
      * @param   string  $string
@@ -24,11 +24,10 @@ class Template {
      */
     public function render(string $string, array $params) : string
     {
-        // Deletions from the string
+        // Deletions from the string. First be done *first*
         $string = $this->deleteComments($string);
 
-        // Simple variable printing
-        $string = $this->renderConditionals($string, $params);
+        // Simple variable printing. Must be done *last*
         $string = $this->renderVars($string, $params);
 
         return $string;
@@ -44,21 +43,11 @@ class Template {
      *
      * @return  string
      */
-    private function renderVars(string $string, array $params) : string {
-        return $string;
-    }
-
-    /**
-     * Searches for conditionals in the string and prints the variable inside
-     * of it, is the prefixed condition is true.
-     * Conditional tags have the following syntax: {{condition?variable}}
-     *
-     * @param   string  $string
-     * @param   array   $params
-     *
-     * @return  string
-     */
-    private function renderConditionals(string $string, array $params) : string {
+    private function renderVars(string $string, array $params) : string
+    {
+        foreach ($params as $key => $value) {
+            $string = str_replace('{{'.$key.'}}', $value, $string);
+        }
         return $string;
     }
 
@@ -71,6 +60,6 @@ class Template {
      * @return  string
      */
     private function deleteComments(string $string) : string {
-        return preg_replace('#'.$this->comment.'#', '', $string);
+        return preg_replace($this->comment, '', $string);
     }
 }
